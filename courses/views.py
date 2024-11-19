9859190a21d10b8f34b7005d1e946901aa5f4890
Georgiy <gogapryst@gmail.com>
2024-11-20 00:22:09 +0300

    add drag-and-drop reordering modules and content

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from .forms import ModuleFormSet
 from django.apps import apps
 from django.forms.models import modelform_factory
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 
 class OwnerMixin:
@@ -153,3 +154,21 @@ class ModuleContentListView(TemplateResponseMixin, View):
             course__owner=request.user
         )
         return self.render_to_response({'module': module})
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(
+                id=id,
+                course__owner=request.user
+            ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(
+                id=id,
+                module__course__owner=request.user
+            ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
