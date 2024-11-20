2cea71f8f156e6d16475dac132e7d28aea5ea0e8
Georgiy <gogapryst@gmail.com>
2024-11-20 21:51:43 +0300

    extend serializers

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
+from django.db.models import Count
+from courses.api.pagination import StandardPagination
 
 
 class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
-    
+
 class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
